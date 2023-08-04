ec6510b98cf4282893eade2fc4d370d5accbce2e
marvellous <mmarvellous69@gmail.com>
2023-08-04 10:22:38 +0400

    2nd commit- front-end added

--- Core/settings.py
+++ Core/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "front-end/dist/front-end"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
